5b9ed615117bc768f6117e72d8837642ab2f6796
Edward Grey <oliverbissell@hotmail.co.uk>
2025-06-02 11:25:48 +0100

    Added Stream File

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,16 +2,14 @@ fx_version 'cerulean'
 
 game 'gta5'
 
-author 'The R'
+author 'The R & Austen64'
 
 description 'For Local People'
 
 files {
-    'handling.meta',
     'carvariations.meta',
     'carcols.meta',
 }
 
-data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
