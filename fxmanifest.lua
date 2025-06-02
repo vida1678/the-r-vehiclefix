ddeb0d4082ca9ae4dd21ac5a0dada6397935eb68
The R <57016118+vida1678@users.noreply.github.com>
2025-06-02 11:22:06 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'The R'
+
+description 'For Local People'
+
+files {
+    'handling.meta',
+    'carvariations.meta',
+    'carcols.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
